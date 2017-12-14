7e62059192aec1f4eadb48e791874714efae43f1
lqq <lqq@gaodig.com>
2017-12-14 14:37:56 +0800

    add vedios

---
+++ routes/shareVedio.js
@@ -0,0 +1,35 @@
+// shareVedio
+var express = require('express');
+var router = express.Router();
+let  fs = require('fs');
+let  join = require('path').join;
+
+router.get('/', function(req, res, next) {
+      res.render('shareVedio');
+});
+
+
+router.get('/getDataList', function(req, res, next) {
+    // 获得资源文件下的所有文件名 并返回数组
+    let fileNames=findSync('./public/vedios');
+    res.send(fileNames)
+});
+
+function findSync(startPath) {
+    let result=[];
+    function finder(path) {
+        let files=fs.readdirSync(path);
+        files.forEach((val,index) => {
+            let fPath=join(path,val);
+            let stats=fs.statSync(fPath);
+            if(stats.isDirectory()) finder(fPath);
+            if(stats.isFile()) result.push(val);
+        });
+
+    }
+    finder(startPath);
+    return result;
+}
+
+
+module.exports = router;
